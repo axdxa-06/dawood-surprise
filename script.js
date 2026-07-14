fcd1a1376eef96919bc4b33fbaaca0ce7a4ac7f0
axdxa-06 <khawaja.ada@gmail.com>
2026-07-14 14:30:51 -0400

    Add heart trail

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const engineButton = document.querySelector(".engine-button");
 const car = document.querySelector(".car-wrapper");
+const hearts = document.querySelector(".heart-trail");
 
 engineButton.addEventListener("click", () => {
     engineButton.style.display = "none";
+
+    hearts.style.opacity = "1";
     car.classList.add("drive-away");
 });
