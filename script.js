39cc50db0f68f574354232a2e3796ed20cf72a20
axdxa-06 <khawaja.ada@gmail.com>
2026-07-14 01:36:36 -0400

    Button engine drive

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const engineButton = document.querySelector(".engine-button");
+const car = document.querySelector(".car-wrapper");
 
 engineButton.addEventListener("click", () => {
     engineButton.style.display = "none";
+    car.classList.add("drive-away");
 });
