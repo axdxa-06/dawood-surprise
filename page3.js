623a07893818f44b05ca03877a14009811a53ee7
axdxa-06 <khawaja.ada@gmail.com>
2026-07-18 00:42:11 -0400

    Add close button

--- page3.js
+++ page3.js
@@ -1,18 +1,32 @@
+console.log("Page 3 JS loaded");
+
+
 const envelopes = document.querySelectorAll(".envelope");
+
+
+console.log("Envelopes found:", envelopes.length);
+
 
 
 envelopes.forEach((envelope) => {
 
     envelope.addEventListener("click", () => {
 
+
         const memoryNumber = envelope.dataset.memory;
+
 
         const memoryScreen = document.getElementById(
             "memory" + memoryNumber
         );
 
 
-        memoryScreen.classList.add("show");
+        if (memoryScreen) {
+
+            memoryScreen.classList.add("show");
+
+        }
+
 
     });
 
@@ -20,15 +34,25 @@ envelopes.forEach((envelope) => {
 
 
 
+
+
 const closeButtons = document.querySelectorAll(".close-memory");
 
 
-closeButtons.forEach((button)=>{
 
-    button.addEventListener("click",()=>{
+closeButtons.forEach((button) => {
 
-        button.parentElement.classList.remove("show");
+
+    button.addEventListener("click", () => {
+
+
+        const memoryScreen = button.closest(".memory-screen");
+
+
+        memoryScreen.classList.remove("show");
+
 
     });
+
 
 });
