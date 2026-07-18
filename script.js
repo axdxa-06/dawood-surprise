b65897c77915614b5d37a0c256195f123f300ff1
axdxa-06 <khawaja.ada@gmail.com>
2026-07-17 23:54:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,21 +20,3 @@ reverseButton.addEventListener("touchstart", () => {
     reverseButton.style.transform = `translate(${x}px, ${y}px)`;
 });
 
-console.log("Page 3 JS loaded");
-
-const envelopes = document.querySelectorAll(".envelope");
-
-console.log("Envelopes found:", envelopes.length);
-
-
-envelopes.forEach((envelope) => {
-
-    envelope.addEventListener("click", () => {
-
-        console.log("Envelope clicked");
-
-        envelope.classList.add("open");
-
-    });
-
-});
